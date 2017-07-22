e3f9e48a022dd4f8ee92e19e4260ce4ab85d22af
akatsuki174 <dev2015js@gmail.com>
2017-07-23 08:47:54 +0900

    TabBarControllerを追加

---
+++ AutoLayoutTechniques/ViewControllerTab2.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerTab2.swift
+//  AutoLayoutTechniques
+//
+//  Created by akatsuki174 on 2017/07/23.
+//  Copyright © 2017年 akatsuki174. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTab2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
