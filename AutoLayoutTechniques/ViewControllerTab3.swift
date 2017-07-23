ee53e08e0749e7e8a758b7a44a455fa6503ec450
akatsuki174 <dev2015js@gmail.com>
2017-07-23 10:19:03 +0900

    Proportional propositionを検証するための初期コードを追加

---
+++ AutoLayoutTechniques/ViewControllerTab3.swift
@@ -0,0 +1,16 @@
+//
+//  ViewControllerTab3.swift
+//  AutoLayoutTechniques
+//
+//  Created by akatsuki174 on 2017/07/23.
+//  Copyright © 2017年 akatsuki174. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTab3: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }    
+}
+
