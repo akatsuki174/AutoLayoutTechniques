34af2c5877a29317956b2b6951c55ff4c7403954
akatsuki174 <dev2015js@gmail.com>
2017-07-23 09:08:50 +0900

    インデント調整

--- AutoLayoutTechniques/ViewControllerTab2.swift
+++ AutoLayoutTechniques/ViewControllerTab2.swift
@@ -25,20 +25,20 @@ class ViewControllerTab2: UIViewController {
         NSLayoutConstraint.activate([
             sampleView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
             sampleView.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
-            ])
+        ])
         
         if #available(iOS 11, *) {
             let guide = view.safeAreaLayoutGuide
             NSLayoutConstraint.activate([
                 sampleView.topAnchor.constraintEqualToSystemSpacingBelow(guide.topAnchor, multiplier: 1.0),
                 guide.bottomAnchor.constraintEqualToSystemSpacingBelow(sampleView.bottomAnchor, multiplier: 1.0)
-                ])
+            ])
         } else {
             let standardSpacing: CGFloat = 8.0
             NSLayoutConstraint.activate([
                 sampleView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: standardSpacing),
                 bottomLayoutGuide.topAnchor.constraint(equalTo: sampleView.bottomAnchor, constant: standardSpacing)
-                ])
+            ])
         }
     }
     
