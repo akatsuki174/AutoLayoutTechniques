3388ba7fc199bc41c39771acbbed0d1556b39741
akatsuki174 <dev2015js@gmail.com>
2017-07-24 15:55:12 +0900

    Stack view adaptive layoutの下準備完了

---
+++ AutoLayoutTechniques/ViewControllerTab4.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllerTab4.swift
+//  AutoLayoutTechniques
+//
+//  Created by akatsuki174 on 2017/07/24.
+//  Copyright © 2017年 akatsuki174. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTab4: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
